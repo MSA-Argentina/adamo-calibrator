f5012a4ceef36b95bf758e92dc5d0946a69d1d2b
Nicolás Illia <nicoillia@gmail.com>
2014-05-12 14:39:40 -0300

    working with frontend

--- html/js/calibrator.js
+++ html/js/calibrator.js
@@ -1,8 +1,5 @@
 get_url = get_url_function("calibrator");
 
-var nclicks = 0;
-var pointers = [[100, 100], [200, 200]];
-
 function initiate(){
     var screen_resolution = [screen.width, screen.height];
     send('initiate', screen_resolution);
@@ -13,11 +10,11 @@ function click(e){
     send('click', click_pos);
 }
 
-function move_pointer(x, y){
-    var img_pointer = $("#img_pointer")
+function move_pointer(xy){
+    var img_pointer = $("#img_pointer");
     img_pointer.css('display', 'inline');
-    img_pointer.css('left', x - (parseInt(img_pointer.css('width')) / 2));
-    img_pointer.css('top', y - (parseInt(img_pointer.css('height')) / 2));
+    img_pointer.css('left', xy[0] - (parseInt(img_pointer.css('width')) / 2));
+    img_pointer.css('top', xy[1] - (parseInt(img_pointer.css('height')) / 2));
 }
 
 function get_click_position(e) {
@@ -27,11 +24,17 @@ function get_click_position(e) {
     return [xPosition, yPosition]
 }
 
+function ready() {
+    $("#img_pointer").css('display', 'inline');
+    send('ready');
+}
+
 $(document).ready(function(){
+    //Sending a screen resolution to backend
     var screen_resolution = [screen.width, screen.height];
     send('initiate', screen_resolution);
-    //Mostrar pantalla de inicio
-    //$(document).click(click);
+
+    //Adding click event
     $(document).click(function(e) {
         var click_pos = get_click_position(e);
         send('click', click_pos);
