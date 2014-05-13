e14b468a711b0b9485b27fd166149383cf4fad56
Nicolás Illia <nicoillia@gmail.com>
2014-05-13 10:34:38 -0300

    working with xinput interface

--- xinput.py
+++ xinput.py
@@ -1,12 +1,38 @@
-from evdev import InputDevice, list_devices
+from os import popen
+
 
 class XInput:
 
-    def get_device():
-        devices = map(InputDevice, list_devices())
+    def get_device_with_prop(self, prop):
+        devices = []
+        proc = popen('xinput list --id-only')
+        stdout = proc.read()
+        dev_ids = stdout.split('\n')[0:-1]
 
-    def get_prop():
-        pass
+        for id in dev_ids:
+            proc = popen('xinput list-props %s' % id)
+            stdout = proc.read()
+            if prop in stdout:
+                stdout = stdout.split('\n')
+                for property in stdout:
+                    if prop in property and '<no items>' not in property:
+                        devices.append(id)
+                        break
+        return devices
 
-    def set_prop(self, property, device, data):
-        print "xinput set-prop", device, property, " ".join(data)
+    def get_prop(self, dev_id, prop):
+        value = None
+        proc = popen('xinput list-props  %s' % dev_id)
+        stdout = proc.read()
+        if prop in stdout:
+            stdout = stdout.split('\n')
+            for property in stdout:
+                if prop in property:
+                    value = property.split(':')[1]
+                    value = value.replace('\t', '')
+                    value = value.split(',')
+                    break
+        return value
+
+    def set_prop(self, dev_id, property, data):
+        popen('xinput set-prop {0} {1} {2}'.format(dev_id, property, data))
