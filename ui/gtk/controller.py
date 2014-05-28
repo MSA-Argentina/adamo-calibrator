9ff4beca7d24d0d2e39039bcc7ea4c67a3e50bc7
Nicolás Illia <nicoillia@gmail.com>
2014-05-28 11:15:58 -0300

    improving gtk interface

--- ui/gtk/controller.py
+++ ui/gtk/controller.py
@@ -6,6 +6,7 @@ except ImportError:
 
 import pygtk
 pygtk.require('2.0')
+import pango
 
 from calibrator.calibrator import Calibrator
 from settings import DUALCLICK_THRESHOLD, FINGER_DELTA, MISCLICK_THRESHOLD, \
@@ -15,14 +16,16 @@ from settings import DUALCLICK_THRESHOLD, FINGER_DELTA, MISCLICK_THRESHOLD, \
 class Window():
 
     def __init__(self):
+        gtk.gdk.threads_init()
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        self.window.connect("delete_event", self.close_application)
+        self.window.connect("destroy", gtk.main_quit)
         self.window.set_position(gtk.WIN_POS_CENTER_ALWAYS)
 
         if FULLSCREEN:
             self.window.fullscreen()
 
         self.drawing_area = gtk.DrawingArea()
+        self.drawing_area.connect("expose_event", self.init_dialog)
         self.window.add(self.drawing_area)
         self.window.show_all()
 
@@ -30,7 +33,7 @@ class Window():
         self.gc = self.drawing_area.get_style().fg_gc[gtk.STATE_NORMAL]
 
         self.window.add_events(gtk.gdk.BUTTON_RELEASE_MASK |
-                               gtk.gdk.BUTTON_PRESS_MASK)
+                               gtk.gdk.BUTTON_PRESS_MASK | gtk.gdk.EXPOSE)
         self.window.connect('button-press-event', self.on_button_pressed)
         self.window.connect('button-release-event', self.on_button_released)
 
@@ -43,7 +46,7 @@ class Window():
 
         self.calibrator.set_screen_prop(self.width, self.height)
 
-        self.status = None
+        self.status = (None, None)
         self.next = (0, 0)
 
         gtk.main()
@@ -52,10 +55,12 @@ class Window():
         drawable = self.drawable
         gc = self.gc
 
-        x1 = centerx - 24
-        x2 = centerx + 24
-        y1 = centery - 24
-        y2 = centery + 24
+        size = 48
+
+        x1 = centerx - size / 2
+        x2 = centerx + size / 2
+        y1 = centery - size / 2
+        y2 = centery + size / 2
         r = 10
 
         drawable.draw_line(gc, centerx, y1, centerx, y2)
@@ -67,23 +72,24 @@ class Window():
         return False
 
     def on_button_pressed(self, widget, event):
-        if event.button == 1 and self.status == 'calibrating':
+        if event.button == 1 and self.status[0] is not 'init':
             data = (event.x_root, event.y_root)
             error = self.calibrator.add_click(data)
             if error is None:
                 print "Click valid: ", data
                 self.next = self.calibrator.get_next_point()
+                self.status = ('calibrating', None)
             elif error == 'misclick':
-                print "Misclick detected: ", data
-                self.error("Misclick detected: " + str(data))
-                #self.send_command('misclick', data)
+                msg = "Misclick detected"
+                print msg, ':', data
+                self.error(msg)
             elif error == 'doubleclick':
-                print "Doubleclick detected: ", data
-                self.error("Doubleclick detected: " + str(data))
-                #self.send_command('doubleclick', data)
+                msg = "Doubleclick detected"
+                print msg, ':', data
+                self.error(msg)
             return True
         else:
-            self.status = 'calibrating'
+            self.status = ('calibrating', None)
             self.next = self.calibrator.get_next_point()
 
     def on_button_released(self, widget, event):
@@ -93,22 +99,41 @@ class Window():
                 self.finish()
                 quit()
             else:
+                if self.status[0] == 'error':
+                    self.error(self.status[1])
                 self.draw_pointer(self.next)
         return True
 
-    def error(self, msg):
+    def init_dialog(self, widget, event):
+        msg = "Please touch screen for start..."
+        font = 'Helvetica 12'
+        self.status = ('init', msg)
+        self.draw_text(400, 200, font, msg)
+
+    def draw_text(self, width, height, font, text):
+        drawing_area = self.drawing_area
         drawable = self.drawable
         gc = self.gc
 
-        width = 200
-        height = 100
-        font = gtk.load_font(
-            "-misc-fixed-medium-r-*-*-*-140-*-*-*-*-iso8859-1")
+        left = self.width / 2 - width / 2
+        top = self.height / 2 - height / 2
 
-        drawable.draw_rectangle(gc, False, self.width / 2 - width / 2,
-                                self.height / 2 - height / 2, width, height)
-        drawable.draw_text(font, gc, self.width / 2 - len(msg),
-                           self.height / 2 - len(msg), msg)
+        font_desc = pango.FontDescription(font)
+
+        layout = drawing_area.create_pango_layout(text)
+        layout.set_font_description(font_desc)
+        text_width, text_height = layout.get_pixel_size()
+
+        text_left = left + (width - text_width) / 2
+        text_top = top + (height - text_height) / 2
+        drawable.draw_rectangle(gc, False, left, top, width, height)
+        drawable.draw_rectangle(gc, False, left - 2, top - 2, width + 4, height + 4)
+        drawable.draw_layout(gc, text_left, text_top, layout)
+
+    def error(self, msg):
+        self.status = ('error', msg)
+        font = 'Helvetica 12'
+        self.draw_text(200, 100, font, msg)
 
     def finish(self):
         self.calibrator.calc_new_axis()
