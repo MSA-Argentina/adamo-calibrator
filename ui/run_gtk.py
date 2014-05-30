7f9d1cbe9db4630bb8ed17376f7e2c9fc6cbcd34
Nicolás Illia <nicoillia@gmail.com>
2014-05-30 12:11:09 -0300

    improving code

--- ui/run_gtk.py
+++ ui/run_gtk.py
@@ -2,9 +2,8 @@ from ui.gtk.controller import Window
 
 
 def run_gtk(fake=None, device=None, misclick_threshold=None,
-    dualclick_threshold=None, finger_delta=None,
-    timeout=None, fast_start=None):
-
+            dualclick_threshold=None, finger_delta=None,
+            timeout=None, fast_start=None):
     window = Window(fake, device, misclick_threshold, dualclick_threshold,
                     finger_delta, timeout, fast_start)
 
