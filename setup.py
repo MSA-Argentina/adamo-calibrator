16bf4ad275e15e1189e00d1ce0477bcb7158f0c3
Nicolas <nicoillia@gmail.com>
2014-09-25 12:31:40 -0300

    Changing version to 0.0.8

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 setup(
     name='adamo_calibrator',
-    version='0.0.7',
+    version='0.0.8',
     author='Nicolas Illia',
     author_email='nillia@msa.com.ar',
     packages=['adamo_calibrator'],
